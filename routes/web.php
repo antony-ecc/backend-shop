c8b90d135eaabf3a25954c3c5a5c3d68d4f5f434
Antony <antony.ecc@gmail.com>
2025-09-08 08:29:01 +0700

    CRUD Category Done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Route::prefix('admin')->middleware('auth')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::resource('/category', CategoryController::class, ['except' => 'show']);
 });
